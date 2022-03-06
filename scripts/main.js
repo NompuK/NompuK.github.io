7d453ead7495cee0ff85acd0252a15b84aee26f2
grp.data <grp.data@sovendus.com>
2022-03-06 21:55:35 +0100

    javascript to change image on click event

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,11 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src','images/initech.gif');
+    } else {
+      myImage.setAttribute('src','images/initech_building.jpeg');
+    }
+}
+
