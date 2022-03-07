391e696ddc75fab8b279857551469e10f4c0bbd9
NompuK <simon.kupferer@posteo.net>
2022-03-07 21:58:17 +0100

    localStorage function added

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,24 @@ myImage.onclick = function() {
     }
 }
 
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+	let myName = prompt('Bitte gib deinen Namen ein:');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Hallo ' + myName +'! Wie geht es dir?';
+}
+
+if(!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	let storedName = localStorage.getItem('name');
+	myHeading.textContext = 'Hallo ' + storedName + '! Wie geht es dir?';
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
+
+
