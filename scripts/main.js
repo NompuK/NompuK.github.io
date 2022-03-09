bd0acab06b42669981eea81ee19eb14c34708580
NompuK <simon.kupferer@posteo.net>
2022-03-09 22:11:37 +0100

    more javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,41 @@
+
+
+
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute('src','images/initech.gif');
-    } else {
+    if(mySrc === 'images/Initech.gif') {
       myImage.setAttribute('src','images/initech_building.jpeg');
+    } else {
+      myImage.setAttribute('src','images/Initech.gif');
     }
 }
 
-let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
+let myButton = document.querySelector('button');
 
 function setUserName() {
 	let myName = prompt('Bitte gib deinen Namen ein:');
+	if(!myName) {
+		setUserName();
+	} else {
 	localStorage.setItem('name', myName);
 	myHeading.textContent = 'Hallo ' + myName +'! Wie geht es dir?';
+	}	
 }
 
-if(!localStorage.getItem('name')) {
-	setUserName();
-} else {
-	let storedName = localStorage.getItem('name');
-	myHeading.textContext = 'Hallo ' + storedName + '! Wie geht es dir?';
-}
 
 myButton.onclick = function() {
 	setUserName();
 }
 
 
+
+
+if(localStorage.getItem('name') !== null) {
+	setUserName();
+} else {
+	let storedName = localStorage.getItem('name');
+	myHeading.textContext = 'Hallo ' + storedName + '! Wie geht es dir?';
+}
